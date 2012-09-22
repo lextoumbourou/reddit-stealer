7e62141fd83f162fbf48198f194bc3f9df078207
Lex Toumbourou <lextoumbourou@gmail.com>
2012-09-23 01:29:20 +1000

    Updated default config

--- config-default.py
+++ config-default.py
@@ -1,2 +1,11 @@
+# Temporary path for video files
 TMP_PATH = "/tmp"
+
+# Path to ripped wavs
 FINISH_PATH = "~/RippedWavs"
+
+# A tuple of Subreddits from which to download Youtube videos from
+SUBREDDITS = ('ObscureMedia', 'exploitation')
+
+# Number of videos to download at a time
+LIMIT = 50
