c1d435ffd63919e6eb89879ac28782a7910ee0de
Lex Toumbourou <lextoumbourou@gmail.com>
2012-12-31 02:30:01 +1100

    Pep8, force option and audio flag works. A lot of trap music sucks. Damn.

--- reddit-stealer.py
+++ reddit-stealer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env/ python
 import os
 import subprocess
 import json
@@ -7,39 +8,46 @@ import urllib2
 
 import httplib2
 from pytube import YouTube
-""" 
+import sh
+"""
 Reddit Video Stealer
 
-Download every YouTube video in a subreddit at the highest 
+Download every YouTube video in a subreddit at the highest
 possible quality, and, optionally rip the audio.
 
 """
 USER_AGENT = 'reddit-stealer.py by /u/WalkThePlank'
 
+
 def get_args():
     parser = argparse.ArgumentParser(
-            description="Download every video in a subreddit")
+        description="Download every video in a subreddit")
     parser.add_argument(
-            'subreddits', type=str, nargs='+', 
-            help='Subreddits to steal from')
+        'subreddits', type=str, nargs='+',
+        help='Subreddits to steal from')
     parser.add_argument(
-            '-l', '--limit', type=int, default=50,
-            help='Limit amount of videos downloadable per Subreddit')
+        '-l', '--limit', type=int, default=50,
+        help='Limit amount of videos downloadable per Subreddit')
     parser.add_argument(
-            '-o', '--output_dir', type=str, default='./',
-            help='Specify output directory (defaults to current directory)')
+        '-o', '--output_dir', type=str, default='./',
+        help='Specify output directory (defaults to current directory)')
     parser.add_argument(
-            '-a', '--audio', action='store_true',
-            help='Specify output directory (defaults to current directory)')
+        '-a', '--audio', action='store_true',
+        help='Additionally, rip a .wav file when done?')
+    parser.add_argument(
+        '-f', '--force', action='store_true',
+        help='Force download even if file already exists in collection')
+
     args = parser.parse_args()
     return args
+
 
 def get_page(url):
     """
     Return html content (a simple wrapper for httplib2)
     """
     output = []
-    headers = {'User-Agent':USER_AGENT}
+    headers = {'User-Agent': USER_AGENT}
     h = httplib2.Http('.cache')
     resp, content = h.request(url, 'GET', headers=headers)
 
@@ -47,6 +55,7 @@ def get_page(url):
         return content
     else:
         return None
+
 
 def get_youtube_links(data):
     """
@@ -64,24 +73,35 @@ def get_youtube_links(data):
 
     return output
 
-def convert_to_audio(infile, path):
+
+def convert_to_audio(infile, outpath=None):
     """
     Call avconv to perform the video conversion
 
     Arguments:
     infile -- an absolute path to a video file
     """
-    outfile = '{0}/{1}.wav'.format(path, yt.filename)
+    if not outpath:
+        outpath = os.path.dirname(infile)
+
+    # Filename with the extension.
+    filename = os.path.basename(infile)
+    # Won't be needing that anymore.
+    filename = ' '.join(filename.split(".")[0:-1])
+
+    outfile = '{0}/{1}.wav'.format(outpath, filename)
     cmd = ['avconv', '-i', infile, outfile]
     subprocess.call(cmd)
 
+
 class ExtendedYouTube(YouTube):
     """
-    ExtendedYoutube adds just the get_highest_quality method to the pytube.Youtube library
+    ExtendedYoutube adds get_highest_quality() to the pytube.Youtube library
     """
     def get_highest_quality(self, extension_pref='mp4'):
         """
-        Take a Youtube instance as input and return the highest available quality
+        Take a Youtube instance as input
+        and return the highest available quality
         """
         highest = 0
         preferred_ext = 'mp4'
@@ -94,6 +114,7 @@ class ExtendedYouTube(YouTube):
                 result = v
 
         return result
+
 
 def main():
     """
@@ -115,21 +136,20 @@ def main():
             if not video:
                 continue
             download_path = '{0}/{1}.{2}'.format(
-                    args.output_dir,
-                    yt.filename,
-                    video.extension)
-            if not os.path.isfile(download_path):
+                args.output_dir,
+                yt.filename,
+                video.extension)
+            if args.force or not os.path.isfile(download_path):
                 try:
                     video.download(path=args.output_dir)
                 except Exception, e:
                     print "Failed to download {0} due to: {1}".format(
-                            yt.filename,
-                            e)
+                        yt.filename,
+                        e)
                     continue
                 if os.path.isfile(download_path) and args.audio:
                     # Convert the video file to audio
                     convert_to_audio(download_path)
-                    
             else:
                 print "Already got {0}".format(yt.filename)
 
